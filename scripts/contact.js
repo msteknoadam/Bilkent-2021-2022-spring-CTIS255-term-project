5731c4c8c42d1421275c0b326c4b596f4043fdf4
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2022-05-15 14:24:38 +0300

    Finalize contact me page

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,3 +1,11 @@
 document.querySelector("#thanks").addEventListener("click", function () {
 	party.confetti(this);
 });
+document.querySelector("#thanks").addEventListener("mouseenter", function () {
+	party.confetti(this);
+});
+document.querySelector("[type=submit]").addEventListener("click", function () {
+	location.href = `/thanks.html?name=${$("#name").val()}&surname=${$("#surname").val()}&email=${$(
+		"#email"
+	).val()}&message=${$("#message").val()}`;
+});
